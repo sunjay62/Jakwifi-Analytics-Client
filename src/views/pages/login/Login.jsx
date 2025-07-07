f8cfaf0436e185fe79672fee2a9b6a187293cffd
sunjay <jaya@tachyon.net.id>
2025-07-07 13:22:02 +0700

    update form login

--- src/views/pages/login/Login.jsx
+++ src/views/pages/login/Login.jsx
@@ -4,13 +4,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Logo from '../../../../src/assets/images/logotachyon-old.png';
 import Logo2 from '../../../../src/assets/images/JakWiFi-logo.png';
+import Wallpaper from '../../../../src/assets/images/wallpaper.jpg';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -108,7 +109,7 @@ export default function Login() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: { Wallpaper },
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
             backgroundSize: 'cover',
@@ -167,7 +168,7 @@ export default function Login() {
                 {loading ? 'Signing In...' : 'Sign In'}
               </Button>
               <Grid container>
-                <Grid item xs>
+                {/* <Grid item xs>
                   <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
@@ -176,7 +177,7 @@ export default function Login() {
                   <Link href="#" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
-                </Grid>
+                </Grid> */}
               </Grid>
               <div className="copyRightContainer">
                 <Copyright sx={{ mt: 5 }} />
